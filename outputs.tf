8b2cae0853508c4453628ad56f722fd888eeb142
stekern <stekern@users.noreply.github.com>
2021-05-31 10:59:41 +0200

    Add an output for the name of the Lambda function

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "lambda_function_arn" {
   value = aws_lambda_function.infra_trigger_pipeline.arn
 }
+
+output "function_name" {
+  value = aws_lambda_function.infra_trigger_pipeline.function_name
+}
